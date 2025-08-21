552d7395aae8f6ca17b8602a2fdab8965fe3f9b6
Macweese <50101641+Macweese@users.noreply.github.com>
2025-08-21 11:11:54 +0200

    refactor: restructure project

---
+++ frontend/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <h1>Customer Management System</h1>
+    <p>Vue 3 frontend is running on localhost:5173</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

3379546276beba4bb06d5c80f8f5d52a7eddbfcd
Macweese <50101641+Macweese@users.noreply.github.com>
2025-08-24 13:10:47 +0200

    add LICENSE headers

--- src/main/java/se/digg/application/service/MockDataService.java
+++ src/main/java/se/digg/application/service/MockDataService.java
@@ -1,3 +1,10 @@
+/*
+ *   Copyright (c) HAN, 2025
+ *   Licensed under the EUPL-1.2-or-later, with extension of article 5
+ *   (compatibility clause) to any licence for distributing derivative works
+ *   that have been produced by the normal use of the Work as a library.
+ *   See the LICENSE file for the full details of EUPL-1.2
+ */
 package se.digg.application.service;
 
 import java.text.Normalizer;

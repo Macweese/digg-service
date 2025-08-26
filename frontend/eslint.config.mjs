e66b8b39402a526a03588eceb89bf69bf881aba6
Macweese <50101641+Macweese@users.noreply.github.com>
2025-08-26 11:29:05 +0200

    linter: upgrade version

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -8,7 +8,7 @@ import css from "@eslint/css";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
   tseslint.configs.recommended,
   pluginVue.configs["flat/essential"],
   { files: ["**/*.vue"], languageOptions: { parserOptions: { parser: tseslint.parser } } },
